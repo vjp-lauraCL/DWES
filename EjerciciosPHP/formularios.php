6d6e8646d7dc0be9bdba0d0da78cdf77acedad6e
Laura Casero Labrador <lcaserol01@informatica.iesvalledeljerteplasencia.es>
2024-09-24 20:55:32 +0200

    formularios

---
+++ EjerciciosPHP/formularios.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formularios</title>
+</head>
+    <h1>Formulario</h1>
+<body>
+   <form method="post" action="formularios2.php">
+    Introduzca su nombre: <input type="text" name="nombre"><br>
+    Introduzca su edad <input type="text" name="edad"><br>
+    <input type="submit" value="Enviar">
+   </form>
+    
+</body>
+</html>
