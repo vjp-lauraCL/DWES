93c62f8e4474ba473ae7dfa85f04cf89bcee6e44
Laura Casero Labrador <lcaserol01@informatica.iesvalledeljerteplasencia.es>
2024-09-26 19:06:35 +0200

    cadenas

--- Cadenas/cadena3.php
+++ Cadenas/cadena3.php
@@ -7,6 +7,9 @@
 </head>
 <body>
     <?php
+    //A partir de una variable que contenga una URL, utiliza la función parse_url
+    //para extrar de la url las siguientes partes y mostrarlas en pantalla:
+    //El protocolo utilizado, el nombre del usuario, el path de la url, el query string de la url.
    $url = 'http://username:password@hostname:9090/path?arg=value#anchor';
    echo parse_url($url,PHP_URL_USER) . "<br>";
    echo parse_url($url,PHP_URL_SCHEME) . "<br>";
--- Cadenas/cadenas1.php
+++ Cadenas/cadenas1.php
@@ -7,27 +7,41 @@
 </head>
 <body>
     <?php
+    //Crea una variable llamada nombre e inicializala con el valor "anaMarial"
     $nombre = 'anaMArial';
+    //Muestra el nombre en pantalla
     echo "Nombre: $nombre<br>";
+    //Muestra la longitud del nombre
     echo strlen($nombre) . "<br>";
+    //Muestra el nombre en mayúsculas
     echo strtoupper($nombre) . "<br>";
+    //Muestra el nombre en minúsculas
     echo strtolower($nombre) . "<br>";
+    //Muestra el nombre con la primera letra en mayúsculas
     echo substr_count($nombre, 'a') . "<br>";
+    //Muestra el código ASCII de la primera letra del nombre
     $ascii = ord($nombre);
     print($ascii);
     echo "<br>";
+    //Muestra la posicion de la primera A existente  en el nombre. Si no hay ninguna mostrará -1
     echo strpos($nombre, "A");
     echo "<br>";
+    //Lo mismo pero con la última A
     echo strcspn($nombre, "a");
     echo "<br>";
+    //Muestra el nombre sustituyendo las letras por el número cero.
     echo  str_ireplace('$nombre', '0', '$nombre');
     echo "<br>";
+    //Indica si el nombre comienza por "al" o no. 
     if (str_ends_with($nombre, "al") == true) {
         echo " si ";
     }
     echo "<br>";
+    //Elimina los espacios del principio y el final del nombre si los hubiera y muestra el resultado por pantalla
     echo rtrim($nombre);
     echo "<br>";
+    //Muestra la variable nombre con la primera letra en mayúsculas y las demás en minúsculas
+    echo ucfirst(strtolower($nombre));
     ?>
 </body>
 </html>
