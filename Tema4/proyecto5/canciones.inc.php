e34f35ed99bf498044b5184404d394b1e585316e
vjp-lauraCL <lcaserol01@informatica.iesvalledeljerteplasencia.es>
2024-10-06 13:53:32 +0200

    ejercicio

---
+++ Tema4/proyecto5/canciones.inc.php
@@ -0,0 +1,12 @@
+<?php
+function obtenerCanciones(){
+    $canciones = [
+    ["titulo" => "Witch's Spell", "Género"=> "Rock", "Album"=> "Power Up"], 
+    ["titulo" => "Tutto con te", "Género"=> "Pop", "Album"=> "Gira, il mondo gira"], 
+    ["titulo" => "Somethin' Stupid", "Género"=> "Jazz", "Album"=> "The World We Knew"], 
+    ["titulo" => "Cofessin' The Blues", "Género"=> "Blues", "Album"=> "Deuces Wild"],   
+];
+    return $canciones;
+}
+
+?>
