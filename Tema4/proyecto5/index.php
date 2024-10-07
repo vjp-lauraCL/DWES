b21a97a6e4b363df3420ee639ade440816493bfe
vjp-lauraCL <lcaserol01@informatica.iesvalledeljerteplasencia.es>
2024-10-07 13:06:19 +0200

    php

--- Tema4/proyecto5/index.php
+++ Tema4/proyecto5/index.php
@@ -25,8 +25,42 @@
     </form>
    
     <?php
-    ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $textoBusqueda = $_POST['textoBusqueda'];
+        $tipoBusqueda = $_POST['tipoBusqueda'];
+        $genero = $_POST['genero'];
 
+        $listaCanciones = obtenerCanciones();
+        $resultado = [];
+
+        foreach($listaCanciones as $cancion){
+            if($genero && $cancion['genero'] != $genero){
+                continue;
+            }
+            if($tipoBusqueda == 'titulo' && strpos($cancion['titulo'], $textoBusqueda) !== false){
+                $resultado[] = $cancion;    
+            }elseif($tipoBusqueda == 'nombre' && strpos($cancion['nombre'], $textoBusqueda) !== false){
+                $resultado[] = $cancion;
+            }elseif($tipoBusqueda == 'ambosCampos' && (strpos($cancion['titulo'], $textoBusqueda) !== false || strpos($cancion['nombre'], $textoBusqueda) !== false)){
+                $resultado[] = $cancion;
+            }
+        }
+
+        if(!empty($resultado)){
+            echo '<table border="1">';
+            echo '<tr><th>Titulo</th><th>Genero</th><th>Album</th></tr>';
+            foreach($resultado as $cancion){
+                echo '<tr>';
+                echo '<td>' . $cancion['titulo'] . '</td>';
+                echo '<td>' . $cancion['genero'] . '</td>';
+                echo '<td>' . $cancion['album'] . '</td>';
+                echo '</tr>';
+            }
+            echo '</table>';
+        }
+        else{
+            echo 'No hay conicidencias con su búsqueda';
+        }
     }
     ?>
 </body>
